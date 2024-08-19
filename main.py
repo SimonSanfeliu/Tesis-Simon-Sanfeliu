8845acce8275a9da8bbf4b1d6985ac0f82b6f500
Simón Sanfeliú <sanfeliu.simon@gmail.com>
2024-08-19 00:53:05 -0400

    ♻️ ✨ Refactored code to new structure and added engine for queries

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-from process import api_call
-from ragStep import rag_step
-from config import OPENAI_KEY, ANTHROPIC_KEY
+import requests
+import sqlalchemy as sa
+
+from pipeline.process import api_call
+from pipeline.ragStep import rag_step
+from secret.config import OPENAI_KEY, ANTHROPIC_KEY, GOOGLE_KEY, SQL_URL
+
+# Setup params for query engine
+params = requests.get(SQL_URL).json()['params']
+engine = sa.create_engine(f"postgresql+psycopg2://{params['user']}:{params['password']}@{params['host']}/{params['dbname']}")
+engine.begin()
 
 # TODO: Add the entire pipeline here
