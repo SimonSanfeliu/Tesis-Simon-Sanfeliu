36cf127d5c1fef166e756f141bb1f636f20c7622
Simón Sanfeliú <sanfeliu.simon@gmail.com>
2024-08-19 00:51:40 -0400

    🚧 ✨ 🚚 Added Gemini model, format for subqueries and function to run queries

---
+++ pipeline/process.py
@@ -0,0 +1,130 @@
+import pandas as pd
+import openai
+import anthropic
+import google.generativeai as genai
+
+
+def api_call(api_key, model, max_tokens, prompt):
+    """Create the API calls for the LLM to use.
+
+    Args:
+        api_key (str): Key of the API
+        model (str): Name of the model
+        max_tokens (int): The maximum number of tokens used for the response 
+        of the API
+        prompt (str): Prompt for the model
+    
+    Return:
+        response (str): The response from the API
+    """
+    if "gpt" in model:
+        try:
+            client = openai.OpenAI(api_key=api_key)
+            response = client.chat.completions.create(
+                model=model,
+                temperature=0,
+                max_tokens=max_tokens,
+                messages=[
+                    {"role": "user", "content": prompt}
+                ]
+            )
+            response = response.choices[0].message.content
+        except Exception as e:
+            print(f"The following exception occured: {e}")
+            raise Exception(e)
+        
+    elif "claude" in model:
+        try:
+            client = anthropic.Anthropic(api_key=api_key)
+            response = client.messages.create(
+                model=model,
+                temperature=0,
+                max_tokens=max_tokens,
+                messages=[
+                    {"role": "user", "content": prompt}
+                ]
+            )
+            response = response.content[0].text
+        except Exception as e:
+            print(f"The following exception occured: {e}")
+            raise Exception(e)
+    
+    elif "gemini" in model:
+        try:
+            genai.configure(api_key=api_key)
+            generation_config = {
+            "temperature": 0,
+            "max_output_tokens": max_tokens
+            }
+            model2use = genai.GenerativeModel(
+            model_name=model,
+            generation_config=generation_config
+            )
+            chat_session = model2use.start_chat(history=[])
+            response = chat_session.send_message(prompt)
+            response = response.text
+        except Exception as e:
+            print(f"The following exception occured: {e}")
+            raise Exception(e)
+    else:
+        raise Exception("No valid model")
+    
+    return response
+
+
+def format_response(specified_format, response):
+    """Format the response accordingly
+
+    Args:
+        specified_format (str): The type of formatting to use. It can be 
+        'singular' for a singular query string or 'var' for the 
+        decomposition in variables
+        response (str): The response from the LLM
+        
+    Return:
+        formatted_response (str or list): The response ready to be used in the
+        database. A string if the specified format is 'singular', list of the 
+        sub-queries if the format is 'var'
+    """
+    if specified_format == "singular":
+        formatted_response = response.split("```sql")[1].split("```")[0] \
+        .replace("```", "").replace("```sql", "")
+    elif specified_format == "var":
+        formatted_response = response.split("```python")[1].split("```")[0] \
+        .replace("```", "").replace("```python", "")
+        formatted_response = formatted_response.split("\n\n")
+    else:
+        raise Exception("No valid format specified")
+    
+    return formatted_response
+
+
+def run_query(specified_format, formatted_response, engine):
+    """Function to run the SQL query in the database
+
+    Args:
+        specified_format (str): The type of formatting to use. It can be 
+        'singular' for a singular query string or 'var' for the 
+        decomposition in variables
+        formatted_response (str or list): The response ready to be used in the
+        database. A string if the specified format is 'singular', list of the 
+        sub-queries if the format is 'var'
+        engine (SQL object): The engine to access the database
+        
+    Return:
+        results (pd.DataFrame or list): Pandas DataFrame with the results of 
+        the query if specified format is 'singular'. List of DataFrames with 
+        the results of the subqueries and total query if specified format is 
+        'var'
+    """
+    if specified_format == "singular":
+        results = pd.read_sql_query(formatted_response, con=engine)
+    elif specified_format == "var":
+        results = []
+        for query in formatted_response:
+            query = exec(query)
+            results.append(pd.read_sql_query(query, con=engine))
+    else:
+        raise Exception("No valid format specified")
+    
+    return results
