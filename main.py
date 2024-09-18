99d9d4d0b9e97be67b7f1e628def952987344fc6
Simón Sanfeliú <sanfeliu.simon@gmail.com>
2024-09-18 00:33:18 -0300

    ✨ Added run_pipeline

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ import requests
 import sqlalchemy as sa
 import pandas as pd
 
-from pipeline.process import api_call, format_response, schema_linking, classify, decomposition
+from pipeline.process import api_call, format_response, schema_linking, classify, decomposition, run_query
 from pipeline.ragStep import rag_step
+from prompts.correction.SelfCorrection import prompt_self_correction_v2, general_context_selfcorr_v1
 from secret.config import SQL_URL
 
 # Setup params for query engine
 params = requests.get(SQL_URL).json()['params']
 engine = sa.create_engine(f"postgresql+psycopg2://{params['user']}:{params['password']}@{params['host']}/{params['dbname']}")
 engine.begin()
-
-# TODO: Update the entire pipeline here
 
 
 def pipeline(query, model, max_tokens, size, overlap, quantity, format):
@@ -33,9 +32,9 @@ def pipeline(query, model, max_tokens, size, overlap, quantity, format):
     Returns:
         table (str): Resulting SQL query
         usage (dict): API usage after RAG process
-    """
-    # TODO: Add self correction
-    
+        prompts (dict): Dictonary with the prompts used in every step of the 
+        pipeline
+    """    
     # Context for the RAG process
     with open('prompts/schema_linking/DBSchema.txt', 'r') as file:
         context = file.read()
@@ -107,6 +106,8 @@ def recreated_pipeline(query, model, max_tokens, format):
     Returns:
         table (str): Resulting SQL query
         total_usage (dict): API usage after the whole process
+        prompts (dict): Dictonary with the prompts used in every step of the 
+        pipeline
     """
     # Schema linking to obtain the tables needed for the query
     tables, schema_usage = schema_linking(query, model)
@@ -140,8 +141,100 @@ def recreated_pipeline(query, model, max_tokens, format):
     }
     
     return table, total_usage, prompts
-    
+
+
+def run_pipeline(query, model, max_tokens, size, overlap, quantity, format, engine, new_pipe, self_corr):
+    """Function to run the entire pipeline. This pipeline could be the 
+       original one or the new one. Here the self-correction is applied.
+
+    Args:
+        query (str): Natural language query for the database
+        model (str): LLM to use
+        max_tokens (int): Maximum amount of output tokens of the LLM
+        size (int): Size of the chunks for the character text splitter used in
+        the RAG process
+        overlap (int): Size of the overlap of the chunks used in the RAG 
+        process
+        quantity (int): The amount of most similar chunks to consider in the
+        RAG process
+        format (str): The type of formatting to use. It can be 'singular' for
+        a singular query string or 'var' for the decomposition in variables
+        engine (SQL engine): SQL database engine  TODO: Check datatype
+        new_pipe (bool): Condition to use the new pipeline
+        self_corr (bool): Condition to use self-correction
         
+    Returns:
+        result (pandas.DataFrame): Dataframe with the resulting table
+        total_usage (dict): API usage after the whole process
+        prompts (dict): Dictonary with the prompts used in every step of the 
+        pipeline
+    """
+    # Check if the new pipeline is being used
+    if new_pipe:
+        table, total_usage, prompts = pipeline(query, model, max_tokens, size, overlap, quantity, format)
+        # If self-correction is enabled, use the respective prompts to correct
+        if self_corr:
+            try:
+                result = run_query(format, table, engine)
+            except Exception as e:
+                print(f"Raised exception: {e}")
+                print("Start retry with self-correction")
+                tab_schema = prompts["Classification"].split("\n The following tables are needed to generate the query: ")[1]
+                corr_prompt = prompt_self_correction_v2(
+                    gen_task=general_context_selfcorr_v1, 
+                    tab_schema=tab_schema, 
+                    req=query, 
+                    sql_pred=table, 
+                    error=str(e))
+                new, new_usage = api_call(model, max_tokens, corr_prompt)
+                total_usage["Self-correction"] = new_usage
+                prompts["Self-correction"] = corr_prompt
+                try:
+                    result = run_query(format, new, engine)
+                except Exception as e:
+                    raise Exception(f"Failed again: {e}")
+
+        # W/o self-correction
+        else:
+            try:
+                result = run_query(format, table, engine)
+            except Exception as e:
+                raise Exception(f"Raised exception: {e}")
+
+    # Using the recreated pipeline
+    else:
+        table, total_usage, prompts = recreated_pipeline(query, model, max_tokens, format)
+        # If self-correction is enabled, use the respective prompts to correct
+        if self_corr:
+            try:
+                result = run_query(format, table, engine)
+            except Exception as e:
+                print(f"Raised exception: {e}")
+                print("Start retry with self-correction")
+                tab_schema = prompts["Classification"].split("\n The following tables are needed to generate the query: ")[1]
+                corr_prompt = prompt_self_correction_v2(
+                    gen_task=general_context_selfcorr_v1, 
+                    tab_schema=tab_schema, 
+                    req=query, 
+                    sql_pred=table, 
+                    error=str(e))
+                new, new_usage = api_call(model, max_tokens, corr_prompt)
+                total_usage["Self-correction"] = new_usage
+                prompts["Self-correction"] = corr_prompt
+                try:
+                    result = run_query(format, new, engine)
+                except Exception as e:
+                    raise Exception(f"Failed again: {e}")
+
+        # W/o self-correction
+        else:
+            try:
+                result = run_query(format, table, engine)
+            except Exception as e:
+                raise Exception(f"Raised exception: {e}")
+            
+    return result, total_usage, prompts
+
 
 if __name__ == '__main__':
     query = "Give me all the SNe that were first detected between december first 2022 and september first 2023"
