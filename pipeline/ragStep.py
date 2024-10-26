ce77129db945c2a71fbb65ab44b6e2cbe9d9228f
Simón Sanfeliú <sanfeliu.simon@gmail.com>
2024-10-26 00:40:34 -0300

    ✨ Added token usage and cost for RAG step

--- pipeline/ragStep.py
+++ pipeline/ragStep.py
@@ -1,8 +1,9 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.docstore.document import Document
+from langchain.chains.question_answering import load_qa_chain
 from langchain_openai import OpenAIEmbeddings, OpenAI
 from langchain_community.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
+from langchain_community.callbacks.manager import get_openai_callback
 
 from secret.config import OPENAI_KEY
 
@@ -17,12 +18,16 @@ def rag_step(size, overlap, context, ragInstruction, quantity):
     Args:
         size (int): Size of the chunks for the character text splitter
         overlap (int): Size of the overlap of the chunks
-        context (str): Text to be splitted and then retrieve the most important information
-        ragInstruction (str): The instruction given to the RAG process to search for in the context
+        context (str): Text to be splitted and then retrieve the most important
+        information
+        ragInstruction (str): The instruction given to the RAG process to 
+        search for in the context
         quantity (int): The amount of most similar chunks to consider
     
     Returns:
-        A summary made from documents more similar to the described instruction (str)
+        final_summary["output_text"] (str): A summary made from documents more 
+        similar to the described instruction
+        usage (dict): Dictionary with the tokens and costs of the RAG step
     """
     # Create an OpenAI model instance with LangChain (model and embeddings)
     try:
@@ -40,7 +45,10 @@ def rag_step(size, overlap, context, ragInstruction, quantity):
     split_content = splitter.split_text(context)
     for chunk in split_content:
         # Append the split content to the list
-        processed_contents.append(Document(page_content=chunk, metadata={"source": "manual_description"}))
+        processed_contents.append(Document(page_content=chunk, 
+                                           metadata={
+                                               "source": "manual_description"
+                                               }))
 
     # Indexing the fragments using FAISS
     index = FAISS.from_documents(processed_contents, embeddings)
@@ -60,7 +68,16 @@ def rag_step(size, overlap, context, ragInstruction, quantity):
         "question": ragInstruction
     }
 
-    # Final summary
-    final_summary = qa_chain.invoke(inputs)
+    with get_openai_callback() as cb:
+        # Final summary
+        final_summary = qa_chain.invoke(inputs)
+        
+        # Token usage
+        usage = {
+            "input_tokens": cb.prompt_tokens,
+            "output_tokens": cb.completion_tokens,
+            "total_tokens": cb.total_tokens,
+            "total_cost": cb.total_cost
+        }
 
-    return final_summary["output_text"]
+    return final_summary["output_text"], usage
