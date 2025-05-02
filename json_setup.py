7ebef06dd986ae0483239c8e7f88558cd6bb5f74
Simón Sanfeliú <sanfeliu.simon@gmail.com>
2025-05-01 20:15:30 -0400

    ✨ Saving prompts and their version in JSON file

---
+++ json_setup.py
@@ -0,0 +1,82 @@
+import json
+
+from pipeline.process import *
+from prompts.base.prompts import *
+
+# Prompt dictionary guideline and used by Jorge
+prompts = {
+    "Schema Linking": {
+        "base_prompt": tables_linking_prompt_V2,
+        "context1": schema_all_cntxV1,
+        "context2": schema_all_cntxV2_indx,
+        "context3": schema_all_cntxV2,
+    },
+    "Classify": {
+        "base_prompt": diff_class_prompt_v7,
+        "final_instructions": final_instructions_diff_v2
+    },
+    "Decomposition": {
+        "simple": {
+            "query_task": simple_query_task_v2,
+            "query_context": simple_query_cntx,
+            "external_knowledge": "placeholder",
+            "domain_knowledge": "placeholder",
+            "query_instructions": simple_query_instructions_v2
+        },
+        "medium": {
+            "decomp_plan": {
+                "base_prompt": medium_decomp_prompt,
+                "decomp_task": medium_decomp_task_v3 + gpt4turbo1106_decomposed_prompt_2,
+                "query_context": medium_query_cntx,
+                "query_instructions": medium_query_instructions_1_v2
+            },
+            "decomp_gen": {
+                "sql": {
+                    "base_prompt": medium_decomp_gen,
+                    "query_task": medium_query_task_v2,
+                    "query_instructions": medium_query_instructions_2_v2,
+                },
+                "python": {
+                    "base_prompt": "placeholder",
+                    "query_task": "placeholder",
+                    "query_instructions": "placeholder",
+                }
+            }
+        },
+        "advanced": {
+            "decomp_plan": {
+                "base_prompt": adv_decomp_prompt,
+                "decomp_task": adv_decomp_task_v3 + gpt4turbo1106_decomposed_prompt_2,
+                "query_context": adv_query_cntx,
+                "query_instructions": adv_query_instructions_1_v3
+            },
+            "decomp_gen": {
+                "sql": {
+                    "base_prompt": adv_decomp_gen,
+                    "query_task": adv_query_task_v2,
+                    "query_instructions": adv_query_instructions_2_v3,
+                },
+                "python": {
+                    "base_prompt": "placeholder",
+                    "query_task": "placeholder",
+                    "query_instructions": "placeholder",
+                }
+            }
+        }
+    },
+    "Direct": {
+        "base_prompt": {
+            "general_task": general_taskv18,
+            "general_context": general_contextv15,
+            "final_instructions": final_instructions_v19
+        },
+        "request_prompt": {
+            "external_knowledge": "placeholder",
+            "domain_knowledge": "placeholder"
+        }
+    }
+}
+
+# Write the dictionary to a JSON file
+with open("final_prompts/prompts_v1.json", "w", encoding="utf-8") as f:
+    json.dump(prompts, f, ensure_ascii=False, indent=4)
